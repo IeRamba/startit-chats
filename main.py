4d90391087f780522016f587952bcbad3efd2f66
IeRamba <r.ievux@gmail.com>
2020-03-09 16:13:34 +0200

    pielikts sqlalchemy

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import os
 from flask import Flask, json, jsonify, render_template, request
 import chats
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask('app')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
+class test(db.Model):
+  col = db.Column(db.String(255), primary_key=True)
+  col2 = db.Column(db.String(255), unique=True, nullable=False)
 
 @app.route('/')
 def index_lapa():
   return render_template('chats.html')
 
+@app.route('/postgreSQL')
+def postgreSQL():
+  result = test.query.all()
+  return '%r' % result
 
 @app.route('/health')
 def health_check():
